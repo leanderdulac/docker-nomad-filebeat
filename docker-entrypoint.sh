a77456972b8d8cd9215cbbe43fb0f05325da81f7
Brian Clark <brian@clark.zone>
2016-09-21 06:54:37 -0400

    more template updates

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/dumb-init /bin/sh
 set -e
 
-for VAR in $(env | grep ^NOMAD_META_ | cut -d= -f1);do
+for VAR in $(env | grep ^NOMAD_ | cut -d= -f1); do
+  if [ "$VAR" = "NOMAD_MEMORY_LIMIT" ]; then continue; fi
+  if [ "$VAR" = "NOMAD_CPU_LIMIT" ]; then continue; fi
+  if [ "$VAR" = "NOMAD_ALLOC_DIR" ]; then continue; fi
+  if [ "$VAR" = "NOMAD_TASK_DIR" ]; then continue; fi
+
   if [ -n "$META" ]; then
     META="${META},${VAR}"
   else
@@ -9,6 +14,6 @@ for VAR in $(env | grep ^NOMAD_META_ | cut -d= -f1);do
   fi
 done
 
-sigil -f /filebeat.yml.tmpl meta=$META > /filebeat.yml
+sigil -f /filebeat.yml.tmpl nomad_meta=$META > /filebeat.yml
 
 exec "$@"
