06a9a3e27cb0f20dc6afea7d0cfc9b9d259f5807
Brian Clark <brian@clark.zone>
2016-09-21 07:57:48 -0400

    nested fields

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/dumb-init /bin/sh
 set -e
 
-for VAR in $(env | grep ^NOMAD_ | cut -d= -f1); do
-  if [ "$VAR" = "NOMAD_MEMORY_LIMIT" ]; then continue; fi
-  if [ "$VAR" = "NOMAD_CPU_LIMIT" ]; then continue; fi
-  if [ "$VAR" = "NOMAD_ALLOC_DIR" ]; then continue; fi
-  if [ "$VAR" = "NOMAD_TASK_DIR" ]; then continue; fi
+for v in $(env | grep ^NOMAD_ | grep -v ^NOMAD_META_ | cut -d= -f1); do
+  if [ "$v" = "NOMAD_MEMORY_LIMIT" ]; then continue; fi
+  if [ "$v" = "NOMAD_CPU_LIMIT" ]; then continue; fi
+  if [ "$v" = "NOMAD_ALLOC_DIR" ]; then continue; fi
+  if [ "$v" = "NOMAD_TASK_DIR" ]; then continue; fi
 
-  if [ -n "$META" ]; then
-    META="${META},${VAR}"
+  if [ -n "$nomad_vars" ]; then
+    nomad_vars="${nomad_vars},${v}"
   else
-    META="${VAR}"
+    nomad_vars="${v}"
   fi
 done
 
-sigil -f /filebeat.yml.tmpl nomad_meta=$META > /filebeat.yml
+for v in $(env | grep ^NOMAD_META_ | cut -d= -f1); do
+  if [ -n "$meta_vars" ]; then
+    meta_vars="${meta_vars},${v}"
+  else
+    meta_vars="${v}"
+  fi
+done
+
+sigil -f /filebeat.yml.tmpl meta_vars=$meta_vars nomad_vars=$nomad_vars > /filebeat.yml
 
 exec "$@"
